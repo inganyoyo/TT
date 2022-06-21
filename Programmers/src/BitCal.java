547cc870fabffb2026ff19364cbf2320f39b167f
juhyun <juhyun@macbookpro.localdomain>
2022-06-21 20:08:40 +0900

    tttttt

---
+++ Programmers/src/BitCal.java
@@ -0,0 +1,34 @@
+
+
+class BitCal {
+	  public String[] solution(int n, int[] arr1, int[] arr2) {
+	        String[] result = new String[n];
+	        for (int i = 0; i < n; i++) {
+	            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+	        }
+
+	        for (int i = 0; i < n; i++) {
+	            result[i] = String.format("%" + n + "s", result[i]);
+	            result[i] = result[i].replaceAll("1", "#");
+	            result[i] = result[i].replaceAll("0", " ");
+	        }
+
+	        return result;
+	    }
+
+	public static void main(String[] args) {
+
+		BitCal s3 = new BitCal();
+	int[] arr1 = {46, 33, 33 ,22, 31, 50};
+	int[] arr2 = {27 ,56, 19, 14, 14, 10};
+	int n =6;
+	
+	
+		
+		s3.solution(n,arr1,arr2);
+
+	}
+}
+
+//dababcdcd
+//dababcdcd
