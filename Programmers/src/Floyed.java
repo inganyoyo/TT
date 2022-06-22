0a81f1005bdbc0d491525b44bebe32796048768a
inganyoyo <inganyoyo@me.com>
2022-06-22 20:54:36 +0900

    Add files via upload

--- Programmers/src/Floyed.java
+++ Programmers/src/Floyed.java
@@ -8,14 +8,15 @@ public class Floyed {
     // 노드의 개수(N), 간선의 개수(M)
     public static int n =5, m=6;
     // 2차원 배열(그래프 표현)를 만들기
-    public static int[][] graph = new int[101][101];
-
+    public static int[][] graph = new int[n+1][n+1];
+    
+    
     public static void main(String[] args) {
     	
         int[][] road = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
         
         // 최단 거리 테이블을 모두 무한으로 초기화
-        for (int i = 0; i < 101; i++) {
+        for (int i = 0; i <n+1; i++) {
             Arrays.fill(graph[i], INF);
         }
      
